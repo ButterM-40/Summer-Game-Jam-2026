14e501e8d4f546925e8bd260fa64151476d9a671
Ramiro Santos <Ramiro.santosf40@gmail.com>
2026-06-27 16:44:12 -0500

    Scoop & Character

---
+++ helper/character_data.gd
@@ -0,0 +1,8 @@
+extends Resource
+class_name CharacterData
+
+@export var idle: Texture2D
+@export var ask: Texture2D
+@export var happy: Texture2D
+@export var disappointed: Texture2D
+@export var character_name: String
