f69826bafc2ecaea5dbfa52ea46745f7a8d4d9de
Temmehboi <moonjitso@gmail.com>
2026-06-27 19:03:05 -0500

    Delete helper directory

--- helper/character_data.gd
+++
@@ -1,8 +0,0 @@
-extends Resource
-class_name CharacterData
-
-@export var idle: Texture2D
-@export var ask: Texture2D
-@export var happy: Texture2D
-@export var disappointed: Texture2D
-@export var character_name: String
--- helper/scoopable.gd
+++
@@ -1,9 +0,0 @@
-extends CPlayerCursor
-# Called when the node enters the scene tree for the first time.
-@export var CURSOR_ICON: Texture2D = preload("res://assets/alien-pixelated-shape-of-a-digital-game.png")
-@export var on_select_cursor: CursorType
-
-
-func _on_texture_button_pressed() -> void:
-	_change_cursor(CURSOR_ICON, on_select_cursor)
-	pass # Replace with function body.
